043f4c2b8d3340304a208c4975950031e3bf839d
Usuario <Usuario@CPD03-M05>
2024-06-10 15:57:39 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Models/hospedagem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel.Models
+{
+    internal class hospedagem
+    {
+
+        public quarto QuartoSelecionado { get; set; }
+        public int QntAdultos { get; set; }
+        public int QntCriancas { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+        public int Estadia
+        {
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+
+        public double ValorTotal
+        {
+            get
+            }
+            { 
+            
+            } 
+        }
+    }
+}
